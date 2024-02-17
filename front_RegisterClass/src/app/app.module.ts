9ea005b1636e5e5791c3a060127857bd01d245d3
Vinicius Lima <56134435+ViniciusLima93@users.noreply.github.com>
2024-02-17 18:49:52 -0400

    Revert "add graphfrom chartjs and material angular"

--- front_RegisterClass/src/app/app.module.ts
+++ front_RegisterClass/src/app/app.module.ts
@@ -1,29 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-import { CardComponent } from "../components/card/card.component";
-
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
-    declarations: [
-        AppComponent,
-        DashboardComponent,
-        AppComponent,
-       
-    ],
-    providers: [],
-    bootstrap: [AppComponent],
-    imports: [
-        BrowserModule,
-        AppRoutingModule,
-        NoopAnimationsModule,
-        CardComponent
-    ]
+  declarations: [
+    AppComponent,
+    DashboardComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
