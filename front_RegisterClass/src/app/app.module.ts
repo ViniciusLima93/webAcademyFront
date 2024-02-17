a22c9077cad6c93c183a7170af3cbe81942f2202
ViniciusLima93 <vnlima9@gmail.com>
2024-02-17 18:45:40 -0400

    add graphfrom chartjs and material angular

--- front_RegisterClass/src/app/app.module.ts
+++ front_RegisterClass/src/app/app.module.ts
@@ -1,20 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { CardComponent } from "../components/card/card.component";
+
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    DashboardComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        DashboardComponent,
+        AppComponent,
+       
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        NoopAnimationsModule,
+        CardComponent
+    ]
 })
 export class AppModule { }
